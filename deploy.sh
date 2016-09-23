890b7b8dcc77effc26acfe653faa5821ffa89f17
root <root@vps.soknn.ru>
2016-09-23 15:49:05 +0300

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ for i in $(seq 1 3); do
 	git pull origin master
 	if [ -f upgradePg ]; then
 		psql -d bbp -U bbp -w < clean.sql
-		psql -d bbp -U bbp -w < dump.sql
+		#psql -d bbp -U bbp -w < dump.sql
 		rm upgradePg
 	fi
   git add -A
