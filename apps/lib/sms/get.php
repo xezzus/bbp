a9266defb55d2666abea053fd3be0e5fe8c48025
DMITRII LAZAREV <dalazarev@yandex.ru>
2016-12-16 11:02:58 +0000

    get.php edited online with Bitbucket

--- apps/lib/sms/get.php
+++ apps/lib/sms/get.php
@@ -1,13 +1,14 @@
 <?php
-return 0;
-/*return function($hashDevice){
 
-  $db = $this->db->pg();
+return function($hashDevice){
+
+  return '';
+  /*$db = $this->db->pg();
   $sql = "SELECT sms FROM devices WHERE device_hash = :hashDevice";
   $sql = $db->prepare($sql);
   $sql->execute([':hashDevice'=>$hashDevice]);
   $res = $sql->fetch();
-  return ['sms'=> $res];
+  return ['sms'=> $res];*/
 
-}*/
+}
 ?>
